cc03c73298cf29ea2b47f4742a1886cee4ae78c2
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-14 20:18:51 +1000

    Moved phrase generator to separate module

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,38 @@
 module WelcomeHelper
+  # noinspection RubyArgCount
+  def self.generate_phrase
+    @temp_phrase = Phrase.skip(rand(Phrase.count)).first.phrase
+    @temp_phase_array = @temp_phrase.split(/[\s.,' ]/)
+
+    counts = Hash.new 0
+    @temp_phase_array.each do |word|
+      counts[word] += 1
+    end
+
+    @noun_count = counts['#noun'] + 5
+    @adjective_count = counts['#adjective'] + 5
+
+    @noun_array = Array.new(@noun_count)
+    @noun_array.map! {
+        |x| x = Noun.skip(rand(Noun.count)).first.noun
+    }
+
+    @adjective_array = Array.new(@adjective_count)
+    @adjective_array.map! {
+        |x| x = Adjective.skip(rand(Adjective.count)).first.adjective
+    }
+
+
+    @new_phrase = @temp_phrase.dup
+
+    for i in 1..@noun_count
+      @new_phrase.sub!('#noun', @noun_array.pop)
+    end
+
+    for i in 1..@adjective_count
+      @new_phrase.sub!('#adjective', @adjective_array.pop)
+    end
+
+    return @new_phrase
+  end
 end
